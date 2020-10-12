892a90cb8776c2031b2bd492c654d7e33b3ac079
Feng Hao <hiroushifuu@outlook.com>
2020-10-12 16:52:16 +0800

    feat: load settings from .env

--- manage.py
+++ manage.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 import os
 import sys
+import environ
 
 if __name__ == '__main__':
-    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.local')
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.deployment')
+    env = environ.Env(
+        # set casting, default value
+        DEBUG=(bool, False)
+    )
+    environ.Env.read_env(env_file=os.path.join(os.getcwd(), '.env'))
+    RUN_ENV = env.str('RUN_ENV', 'local')
+
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.' + RUN_ENV)
 
     from django.core.management import execute_from_command_line
 
